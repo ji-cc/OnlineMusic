453d1d11fb52fa936fa0528483fdb35601c40b1a
ji-cc <jicc0610@163.com>
2021-03-24 18:29:29 +0800

    提交

--- src/dao/MusicDao.java
+++ src/dao/MusicDao.java
@@ -145,7 +145,6 @@ public class MusicDao {
                 // 说明插入成功
                 return 1;
             }
-
         }catch (SQLException e) {
             e.printStackTrace();
         }finally {
@@ -190,17 +189,19 @@ public class MusicDao {
     }
 
     // 根据id在LoveMusic表单中查询
-    private boolean findLoveMusicOnDel(int id) {
+    private boolean findLoveMusicOnDel(int musicId) {  // music 表单中的id，对应于lovemusic表单中的music_id，所以应该上传musicId
         Connection connection = null;
         PreparedStatement ps = null;
-         ResultSet rs = null;
+        ResultSet rs = null;
         try {
-            String sql = "delete from lovemusic where id=?";
+            String sql = "select * from lovemusic where music_id=?";
             connection = DBUtils.getConnection();
             ps = connection.prepareStatement(sql);
+            ps.setInt(1,musicId);
             rs = ps.executeQuery();
-
-
+            if(rs.next()) {
+                return true;
+            }
         }catch (SQLException e) {
             e.printStackTrace();
         }finally {
@@ -210,27 +211,213 @@ public class MusicDao {
     }
     // 根据id删除LoveMusic表单中的歌单
     private int removeLoveMusicOnDel(int id) {
-
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            String sql = "delete from lovemusic where music_id=?";
+            connection = DBUtils.getConnection();
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1,id);
+            int ret = ps.executeUpdate();
+            if(ret == 1) {
+                return 1;
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.getClose(connection,ps,null);
+        }
+        return 0;
     }
 
 
-    // 测试
+    /**
+     * 添加喜欢的音乐的时候，需要先判断该音乐是否存在
+     * @param musicID
+     * @return
+     * */
+    public boolean findMusicByMusicId(int user_id,int musicID) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            String sql = "select * from lovemusic where user_id=? and music_id=?";
+            connection = DBUtils.getConnection();
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1,user_id);
+            ps.setInt(2,musicID);
+            rs = ps.executeQuery();
+            if(rs.next()) {
+                return true;
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.getClose(connection,ps,rs);
+        }
+        return false;
+    }
+
+
+    /**
+     * 添加音乐到“喜欢”列表中
+     * 用户-》音乐
+     * 多对多
+     * 需要中间表
+     */
+    public static boolean insertLoveMusic (int userId, int musicId) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        // 没有用到 ResultSet
+        try {
+            String sql = "insert into lovemusic(user_id,music_id) value (?,?)";
+            connection = DBUtils.getConnection();
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1,userId);
+            ps.setInt(2,musicId);
+            int ret= ps.executeUpdate();
+            // 判断是否插入成功
+            if (ret == 1) {
+                // 说明插入成功
+                return true;
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.getClose(connection,ps,null);  // 此时的rs为null
+        }
+        return false; // 插入失败返回0
+    }
+
+    /**
+     * @param userId 用户id
+     * @param musicId 歌曲id
+     * @return 返回受影响的行数
+     * 移除当前用户喜欢的这首音乐，因为同一首音乐可能多个用户喜欢
+     */
+    public int removeLoveMusic(int userId,int musicId) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+        try {
+            String sql = "delete from lovemusic where user_id=? and music_id=?";
+            connection = DBUtils.getConnection();
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1,userId);
+            ps.setInt(2,musicId);
+            int ret = ps.executeUpdate();
+            if(ret == 1) {
+                return ret;
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.getClose(connection,ps,null);
+        }
+        return 0;
+    }
+
+    /**
+     * 查询用户喜欢的全部歌单
+     * 返回值：List<Music>
+     * @param user_id
+     * @return
+     */
+    public static List<Music> findLoveMusic(int user_id){
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Music> musicList = new ArrayList<>();
+        try { // 需要的是根据userId得到music的全部信息
+            String sql = "select m.id as music_id,title,singer,time,url,userid from lovemusic lm,music m where lm.music_id=m.id and user_id=?";
+            connection = DBUtils.getConnection();
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1,user_id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Music music = new Music();
+                music.setId(rs.getInt("music_id"));
+                music.setTitle(rs.getString("title"));
+                music.setSinger(rs.getString("singer"));
+                music.setTime(rs.getDate("time"));
+                music.setUrl(rs.getString("url"));
+                music.setUserid(rs.getInt("userid"));
+                musicList.add(music);
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.getClose(connection,ps,rs);
+        }
+        return musicList;
+    }
+
+    /**
+        * 根据关键字查询喜欢的歌单
+        * @param str
+        * @return
+        */
+    public static List<Music> ifMusicLove(String str,int user_id){
+        List<Music> musics = new ArrayList<>();
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            connection = DBUtils.getConnection();
+            //ps = conn.prepareStatement("select*from music where title like '%"+str+"%'");
+            ps = connection.prepareStatement("select m.id as music_id,title,singer,time,url,userid from lovemusic " +
+                    "lm,music m where lm.music_id=m.id and user_id=? and  title like '%"+str+"%'");
+            ps.setInt(1,user_id);
+            rs = ps.executeQuery();
+            while(rs.next()) {
+                Music music = new Music();
+                music.setId(rs.getInt("music_id"));
+                music.setTitle(rs.getString("title"));
+                music.setSinger(rs.getString("singer"));
+                music.setTime(rs.getDate("time"));
+                music.setUrl(rs.getString("url"));
+                music.setUserid(rs.getInt("userid"));
+                musics.add(music);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtils.getClose(connection, ps, rs);
+        }
+        return musics;
+    }
+
+
+
+
+    // 测试   // 测试的时候要在方法名前加 static
     public static void main(String[] args) {
-
-        // 查询全部歌单
-        List<Music> musicList = findMusic();
+        // 根据关键字查询用户喜欢的音乐
+        List<Music> musicList = ifMusicLove("铁",1);
         System.out.println(musicList);
-        // [Music{id=1, title='南方姑娘', singer='赵雷', time=2020-07-21, url='music南方姑娘', userid=1}]
 
-        // 通过id查询歌单
-        Music music = findMusicById(1);
-        System.out.println(music);  // Music{id=1, title='南方姑娘', singer='赵雷', time=2020-07-21, url='music南方姑娘', userid=1}
+//        // 查询用户喜欢的音乐
+//        List<Music> musicList = findLoveMusic(1);
+//        System.out.println(musicList);
 
-        //根据关键字查询歌单
-        List<Music> musicList1 = ifMusic("南方");
-        System.out.println(musicList1);  // [Music{id=1, title='南方姑娘', singer='赵雷', time=2020-07-21, url='music南方姑娘', userid=1}]
-
-        // 上传音乐
-        Insert("铁锤妹妹","c","2021-03-20","music\\铁锤妹妹",1);
+//        // 查询全部歌单
+//        List<Music> musicList = findMusic();
+//        System.out.println(musicList);
+//        // [Music{id=1, title='南方姑娘', singer='赵雷', time=2020-07-21, url='music南方姑娘', userid=1}]
+//
+//        // 通过id查询歌单
+//        Music music = findMusicById(1);
+//        System.out.println(music);  // Music{id=1, title='南方姑娘', singer='赵雷', time=2020-07-21, url='music南方姑娘', userid=1}
+//
+//        //根据关键字查询歌单
+//        List<Music> musicList1 = ifMusic("南方");
+//        System.out.println(musicList1);  // [Music{id=1, title='南方姑娘', singer='赵雷', time=2020-07-21, url='music南方姑娘', userid=1}]
+//
+//        // 上传音乐
+//        Insert("铁锤妹妹","c","2021-03-20","music\\铁锤妹妹",1);
+//
+//        // 添加音乐到“喜欢”列表中
+//        System.out.println(insertLoveMusic(1,2));
     }
 }
